e466d8d0c4036bb923dda016900baa6a0663d46b
Marcus Carvalho <marcus.geekcode@gmail.com>
2018-10-18 14:42:18 -0300

    Implementing the feeds logic using lower objects e.g. mixing javascript and jsx.

--- App.js
+++ App.js
@@ -7,23 +7,36 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Dimensions, Image} from 'react-native';
 
+const width = Dimensions.get('screen').width;
+const height = width;
+
+/*
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+});*/
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    const fotos = [
+      {id: 1, usuario: 'Marcus'},
+      {id: 2, usuario: 'Daniella'},
+      {id: 3, usuario: 'Lizarralde'}
+    ];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{marginTop: 30}}>
+        {fotos.map(foto => 
+          <View key={foto.id}>
+            <Text>{foto.usuario}</Text>
+            <Image source={require('./resources/img/person.png')}
+              style={{width: width, height: height}} />
+          </View>
+        )}
       </View>
     );
   }
